526be3fe403e217f06e1bce1acfeabcaff31739c
Aaron Davidge <w0386112@ad.southeastern.edu>
2022-05-18 14:45:27 -0500

    finished jsx & started style

--- src/App.js
+++ src/App.js
@@ -5,34 +5,52 @@ import axios from 'axios';
 function App() {
 
   const [error, setError] = useState('');
-  const [data, setData] = useState('');
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getData()
   }, []);
 
-  async function getData() {
-
-    await axios('https://api.hatchways.io/assessment/students').then((response) => {
-      setData(response.data);
-    }).catch((error) => {
-      console.error("Error fetching data: ", error);
-      setError(error);
-    }).finally(() => {
-      setLoading(false);
-    });
-
+  function getData() {
+    axios('https://api.hatchways.io/assessment/students')
+      .then((response) => {
+        setData(response.data.students);
+      }).catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      }).finally(() => {
+        setLoading(false);
+      })
   }
+
+  console.log(data);
 
 
   if (loading) return "Loading";
   if (error) return "Error";
 
+
   return (
     <div className="App">
-      {/* <h1>Hello world, your React.js app is working</h1> */}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1 id='page-heading'>Frontend Assessment - Student Profile</h1>
+      {data.map((studentInfo) => {
+        return (
+          <div className='student-info'>
+            <img id='student-pic' src={studentInfo.pic} alt="display" img />
+            <h2 id='student-name'>{studentInfo.firstName}&nbsp;{studentInfo.lastName}</h2>
+            <p> Email: {studentInfo.email}<br></br>
+              Company: {studentInfo.company}<br></br>
+              Skill: {studentInfo.skill}<br></br>
+            </p>
+            <ul className='student-grades'>{studentInfo.grades.map((grades) => {
+              return (
+                <li id='grades'>{grades}</li>
+              )
+            })}</ul>
+          </div>
+        )
+      })}
     </div>
   );
 }
